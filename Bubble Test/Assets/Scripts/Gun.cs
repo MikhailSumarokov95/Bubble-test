22c5cc7e56afa4d7ec5a2693d76fc617ccd4bbcb
MikhailSumarokov <ykpon1995@mail.ru>
2022-10-27 21:25:59 +0500

    0.54
    Add AutoPosition Guns

--- Bubble Test/Assets/Scripts/Gun.cs
+++ Bubble Test/Assets/Scripts/Gun.cs
@@ -9,6 +9,11 @@ public class Gun : MonoBehaviour
     private GameObject _bubble;
     private BubbleCartridge _bubbleCartidge;
 
+    private void Start()
+    {
+        transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.05f, -Camera.main.transform.position.z));
+    }
+
     private void Update()
     {
         if (_bubble == null || _bubbleCartidge.IsMissed) CreateBubbles();
